4765f1123f0835bd0ac2f618cbd342f4b434b17a
Ankesh <Ankesh@LENOVO>
2021-08-30 20:45:30 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Genlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myList = new List<string>();
+            myList.Add("Ms.");
+            myList.Add("Ankita");
+            myList.Add("Kharche");
+
+            //Printing Item
+            foreach (string s in myList)
+                Console.Write(s.ToString() + " ");
+
+            //Sorting List
+            myList.Sort();
+            Console.WriteLine("\n After Sorting");
+            foreach (string s in myList)
+                Console.Write(s.ToString() + " ");
+
+            //Removing Items
+            myList.Remove("Ms.");
+            Console.WriteLine("\nRemoving Ms.");
+            foreach (string s in myList)
+                Console.Write(s.ToString() + " ");
+
+            //Inserting Item in the middle
+            myList.Insert(2, "N.");
+            Console.WriteLine("\nInserting N. at index position 2");
+            foreach (string s in myList)
+                Console.Write(s.ToString() + " ");
+        }
+    }
+    
+}
